fniessen/shell-leuven

commit 9da97960f7dcda91c5b789517565b9807004809a

Refactor code (to source local files, and about coloring stderr)

.zshrc
@@ -8,28 +8,27 @@
 
 # Code:
 
-# Allow local Shell customizations.
-if [ -f "$HOME"/.shellrc_local_before ]; then
-    . "$HOME"/.shellrc_local_before
-fi
+# Source local shell customizations.
+source_local_file() {
+    local file="$HOME/$1"
+    if [ -f "$file" ]; then
+        source "$file"
+    fi
+}
 
-# Allow local Zsh customizations.
-if [ -f "$HOME"/.zshrc_local_before ]; then
-    . "$HOME"/.zshrc_local_before
-fi
+source_local_file ".shellrc_local_before" # Source local shell customizations.
+source_local_file ".zshrc_local_before" # Source local Zsh customizations.
 
-# Coloring stderr.
-STDERRED_ESC_CODE=$'\e[37;1;41m'
-zmodload zsh/system
+# Color stderr.
 color_stderr_red() {
-    # Sysread & syswrite are part of `zsh/system'.
-    emulate -LR zsh
-    while sysread; do
-        syswrite -o 2 "$STDERRED_ESC_CODE$REPLY$terminfo[sgr0]"
+    local stderr_red_esc_code=$'\e[37;1;41m'
+    while IFS= read -r line; do
+        echo -e "${stderr_red_esc_code}${line}\e[0m" >&2
     done
 }
 
-exec 2> >( color_stderr_red )
+# Redirect stderr to color_stderr_red function.
+exec 2> >(color_stderr_red)
 
 # # Don't inherit the value of PS1 from the previous shell (Zsh from Bash).
 # PS1=$'%{\e]0;%d\a%}\n%F{grn}%n@%m %F{yel}%d%f\n%# '
@@ -287,7 +286,7 @@ alias history="history -i 0"
 alias h="history -i 0"
 
 # Use Emacs keybindings.
-bindkey -M emacs
+bindkey -e
 
 # Move by whole words.
 bindkey '\e[1;5C' forward-word          # <C-right>
@@ -296,12 +295,6 @@ bindkey '\e[1;5D' backward-word         # <C-left>
 # Search matching commands from the history.
 bindkey '\e[A' history-beginning-search-backward # <up>
 bindkey '\e[B' history-beginning-search-forward  # <down>
-
-# Insert last word with Alt+. -- cool!  BY DEFAULT!???
-# bindkey '\e.' insert-last-word
-
-# # Insert last word with 'M-.'.
-# bindkey -s '\e.' ' $(!!:0)\n'
 
 bindkey "\e[3~" delete-char             # <delete>
 
@@ -431,17 +424,6 @@ alias -g Gitthisweek=' --since=1.week.ago'
 alias -g Gitthismonth=' --since=1.month.ago'
 alias -g Gitthisyear=' --since=1.year.ago'
 
-# Common configuration.
-if [ -f "$HOME"/.shellrc ]; then
-    . "$HOME"/.shellrc
-fi
-
-# Allow local Zsh customizations.
-if [ -f "$HOME"/.zshrc_local_after ]; then
-    . "$HOME"/.zshrc_local_after
-fi
-
-# Allow local Shell customizations.
-if [ -f "$HOME"/.shellrc_local_after ]; then
-    . "$HOME"/.shellrc_local_after
-fi
+source_local_file ".shellrc" # Source common customizations.
+source_local_file ".zshrc_local_after" # Source local Zsh customizations.
+source_local_file ".shellrc_local_after" # Source local shell customizations.

README.org
@@ -255,15 +255,16 @@ To start directly Zsh when opening /Bash on Ubuntu on Windows/:
 ** Startup (Where are Local customizations?)
 
 #+begin_src shell
-# Allow local Shell customizations.
-if [ -f "$HOME"/.shellrc_local_before ]; then
-    . "$HOME"/.shellrc_local_before
-fi
+# Source local shell customizations.
+source_local_file() {
+    local file="$HOME/$1"
+    if [ -f "$file" ]; then
+        source "$file"
+    fi
+}
 
-# Allow local Bash customizations.
-if [ -f "$HOME"/.bashrc_local_before ]; then
-    . "$HOME"/.bashrc_local_before
-fi
+source_local_file ".shellrc_local_before" # Source local shell customizations.
+source_local_file ".bashrc_local_before" # Source local Bash customizations.
 #+end_src
 
 ** Colors
@@ -726,20 +727,9 @@ bind '"\e\C-u": "cd ..\n"'
 ** Ready for more?
 
 #+begin_src shell
-# Common configuration.
-if [ -f "$HOME"/.shellrc ]; then
-    . "$HOME"/.shellrc
-fi
-
-# Allow local Bash customizations.
-if [ -f "$HOME"/.bashrc_local_after ]; then
-    . "$HOME"/.bashrc_local_after
-fi
-
-# Allow local Shell customizations.
-if [ -f "$HOME"/.shellrc_local_after ]; then
-    . "$HOME"/.shellrc_local_after
-fi
+source_local_file ".shellrc" # Source common customizations.
+source_local_file ".bashrc_local_after" # Source local Bash customizations.
+source_local_file ".shellrc_local_after" # Source local shell customizations.
 #+end_src
 
 * Zsh Configuration
@@ -769,15 +759,16 @@ Examples:
 ** Startup (Where are Local customizations?)
 
 #+begin_src shell
-# Allow local Shell customizations.
-if [ -f "$HOME"/.shellrc_local_before ]; then
-    . "$HOME"/.shellrc_local_before
-fi
+# Source local shell customizations.
+source_local_file() {
+    local file="$HOME/$1"
+    if [ -f "$file" ]; then
+        source "$file"
+    fi
+}
 
-# Allow local Zsh customizations.
-if [ -f "$HOME"/.zshrc_local_before ]; then
-    . "$HOME"/.zshrc_local_before
-fi
+source_local_file ".shellrc_local_before" # Source local shell customizations.
+source_local_file ".zshrc_local_before" # Source local Zsh customizations.
 #+end_src
 
 ** Colors
@@ -788,18 +779,16 @@ Wrap text that goes to file with descriptor ~2~ with proper ANSI escape codes,
 making text red.
 
 #+begin_src shell
-# Coloring stderr.
-STDERRED_ESC_CODE=$'\e[37;1;41m'
-zmodload zsh/system
+# Color stderr.
 color_stderr_red() {
-    # Sysread & syswrite are part of `zsh/system'.
-    emulate -LR zsh
-    while sysread; do
-        syswrite -o 2 "$STDERRED_ESC_CODE$REPLY$terminfo[sgr0]"
+    local stderr_red_esc_code=$'\e[37;1;41m'
+    while IFS= read -r line; do
+        echo -e "${stderr_red_esc_code}${line}\e[0m" >&2
     done
 }
 
-exec 2> >( color_stderr_red )
+# Redirect stderr to color_stderr_red function.
+exec 2> >(color_stderr_red)
 #+end_src
 
 ** Prompt
@@ -1394,7 +1383,7 @@ each key combination in between the two quotes.
 
 #+begin_src shell
 # Use Emacs keybindings.
-bindkey -M emacs
+bindkey -e
 #+end_src
 
 *** Commands For Moving
@@ -1417,14 +1406,6 @@ bindkey '\e[1;5D' backward-word         # <C-left>
 # Search matching commands from the history.
 bindkey '\e[A' history-beginning-search-backward # <up>
 bindkey '\e[B' history-beginning-search-forward  # <down>
-#+end_src
-
-#+begin_src shell
-# Insert last word with Alt+. -- cool!  BY DEFAULT!???
-# bindkey '\e.' insert-last-word
-
-# # Insert last word with 'M-.'.
-# bindkey -s '\e.' ' $(!!:0)\n'
 #+end_src
 
 | Key binding | Meaning                                                                            |
@@ -1666,20 +1647,9 @@ alias -g Gitthisyear=' --since=1.year.ago'
 ** Ready for more?
 
 #+begin_src shell
-# Common configuration.
-if [ -f "$HOME"/.shellrc ]; then
-    . "$HOME"/.shellrc
-fi
-
-# Allow local Zsh customizations.
-if [ -f "$HOME"/.zshrc_local_after ]; then
-    . "$HOME"/.zshrc_local_after
-fi
-
-# Allow local Shell customizations.
-if [ -f "$HOME"/.shellrc_local_after ]; then
-    . "$HOME"/.shellrc_local_after
-fi
+source_local_file ".shellrc" # Source common customizations.
+source_local_file ".zshrc_local_after" # Source local Zsh customizations.
+source_local_file ".shellrc_local_after" # Source local shell customizations.
 #+end_src
 
 * Shell agnostic Configuration

.bashrc
@@ -19,15 +19,16 @@
 #     exec zsh
 # fi
 
-# Allow local Shell customizations.
-if [ -f "$HOME"/.shellrc_local_before ]; then
-    . "$HOME"/.shellrc_local_before
-fi
+# Source local shell customizations.
+source_local_file() {
+    local file="$HOME/$1"
+    if [ -f "$file" ]; then
+        source "$file"
+    fi
+}
 
-# Allow local Bash customizations.
-if [ -f "$HOME"/.bashrc_local_before ]; then
-    . "$HOME"/.bashrc_local_before
-fi
+source_local_file ".shellrc_local_before" # Source local shell customizations.
+source_local_file ".bashrc_local_before" # Source local Bash customizations.
 
 git_info_msg() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
@@ -222,17 +223,6 @@ bind '"\ev": edit-and-execute-command'
 # C-M-u: up-directory
 bind '"\e\C-u": "cd ..\n"'
 
-# Common configuration.
-if [ -f "$HOME"/.shellrc ]; then
-    . "$HOME"/.shellrc
-fi
-
-# Allow local Bash customizations.
-if [ -f "$HOME"/.bashrc_local_after ]; then
-    . "$HOME"/.bashrc_local_after
-fi
-
-# Allow local Shell customizations.
-if [ -f "$HOME"/.shellrc_local_after ]; then
-    . "$HOME"/.shellrc_local_after
-fi
+source_local_file ".shellrc" # Source common customizations.
+source_local_file ".bashrc_local_after" # Source local Bash customizations.
+source_local_file ".shellrc_local_after" # Source local shell customizations.

commit c9758c94fcb2ebeadf1f847aa45c433e02c723b8

Fix bug in color_stderr (introduced by commit 9da9796, on 2023-03-11)

.zshrc
@@ -20,10 +20,13 @@ source_local_file ".shellrc_local_before" # Source local shell customizations.
 source_local_file ".zshrc_local_before" # Source local Zsh customizations.
 
 # Color stderr.
+zmodload zsh/system
 color_stderr_red() {
+    # Sysread & syswrite are part of `zsh/system'.
+    emulate -LR zsh
     local stderr_red_esc_code=$'\e[37;1;41m'
-    while IFS= read -r line; do
-        echo -e "${stderr_red_esc_code}${line}\e[0m" >&2
+    while sysread; do
+        syswrite -o 2 "${stderr_red_esc_code}${REPLY}$terminfo[sgr0]"
     done
 }
 

README.org
@@ -784,10 +784,13 @@ making text red.
 
 #+begin_src shell
 # Color stderr.
+zmodload zsh/system
 color_stderr_red() {
+    # Sysread & syswrite are part of `zsh/system'.
+    emulate -LR zsh
     local stderr_red_esc_code=$'\e[37;1;41m'
-    while IFS= read -r line; do
-        echo -e "${stderr_red_esc_code}${line}\e[0m" >&2
+    while sysread; do
+        syswrite -o 2 "${stderr_red_esc_code}${REPLY}$terminfo[sgr0]"
     done
 }
 